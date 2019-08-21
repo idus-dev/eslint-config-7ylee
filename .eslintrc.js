cc678f665c699d316c3e4f3d284e5fffb406cc08
tylee <taeyouklee@gmail.com>
2019-08-22 00:28:33 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    "extends": [
+        "airbnb",
+        "prettier",
+        "prettier/react"
+    ],
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        // Can I remove these now?
+        "ecmaFeatures": {
+            "impliedStrict": true,
+            "classes": true
+        }
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "jest": true
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-underscore-dangle": [
+            "error", 
+            { 
+                "allow": ["_id"] // mongodb _id key
+            }
+        ], 
+        "react/jsx-filename-extension": [
+            1, 
+            {
+                "extensions": [".js", ".jsx"] 
+            }
+        ],
+        "prettier/prettier": [
+            "error",
+            {
+              "singleQuote": true,
+              "printWidth": 80,
+              "tabWidth": 4,
+              "useTabs": false
+            },
+        ],
+    },
+    "plugins": [
+        "html",
+        "prettier",
+        "react-hooks"
+    ]
+}
